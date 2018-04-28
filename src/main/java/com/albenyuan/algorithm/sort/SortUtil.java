de59b5185b254d6118da597212c6fb1a4420e933
Alben Yuan <albenyuan@aliyun.com>
2018-04-28 17:33:04 +0800

    Insertion Sort, Selection Sort, Bubble Sort

---
+++ src/main/java/com/albenyuan/algorithm/sort/SortUtil.java
@@ -0,0 +1,93 @@
+package com.albenyuan.algorithm.sort;
+
+import java.util.List;
+
+/**
+ * 排序工具类
+ *
+ * @Author Alben Yuan
+ * @Date 2018-04-28 15:11
+ */
+public class SortUtil {
+
+    /**
+     * 插入排序
+     * 已经有一个有序的数据序列。要求在这个有序的数据序列中，插入一个数据后依然有序。
+     * <p>
+     * 1.选择第一个元素作为一个有序组合
+     * 2.从第个元素开始，和已有的有序序列中的元素依次比较，
+     * 3. 待排序元素大于等于有序序列的n个元素
+     *
+     * @param list
+     * @return
+     */
+    public static List<Integer> insertionSort(List<Integer> list) {
+
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            int j = i - 1;
+            int temp = list.get(i);  // 1. 取出带插入的元素
+
+            // 和已存在的有序序列从后向前，依次比较，
+            while ((j >= 0) //  不是第一个元素位置
+                    && temp < list.get(j)) { // 还没有找到合适的位置
+                list.set(j + 1, list.get(j)); // 有序序列中第j个元素，后移一位。
+                j--; // 和前一个与元素比较
+            }
+            list.set(j + 1, temp);  // 带插入元素插入到对应的位置
+        }
+        return list;
+    }
+
+
+    /**
+     * 冒泡排序
+     * 1.
+     *
+     * @param list
+     * @return
+     */
+    public static List<Integer> bubbleSort(List<Integer> list) {
+
+        Integer size = list.size();
+        for (Integer i = 0; i < size; i++) {
+            for (Integer j = 0; j < size - 1 - i; j++) {
+
+                Integer dataJ = list.get(j);
+                Integer dataJNext = list.get(j + 1);
+
+                if (dataJ > dataJNext) {        // 相邻元素两两对比
+                    list.set(j + 1, dataJ);
+                    list.set(j, dataJNext);
+                }
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 选择排序
+     * 选择为排序序列中的最小值，放到第一个位置
+     *
+     * @param list
+     * @return
+     */
+    public static List<Integer> selectionSort(List<Integer> list) {
+        int size = list.size();
+        for (Integer i = 0; i < size - 1; i++) {
+            int minIndex = i; // 取出无序序列的第一个元素作为最小值的下标
+            int min = list.get(i); // 取出无序序列的第一个元素作为最小值
+            for (Integer j = i + 1; j < size; j++) {
+                //遍历后续全部数据
+                Integer dataJ = list.get(j);
+                if (dataJ < min) { // 比较判断，是否为已知最小值
+                    min = dataJ; // 取出最小值
+                    minIndex = j; // 记录最小值下标
+                }
+            }
+            list.set(minIndex, list.get(i)); // 将无序序列第一个元素放到，最小值对应的位置，
+            list.set(i, min); // 将最小值，放到无序的第一个位置
+        }
+        return list;
+    }
+}
--- src/test/java/com/albenyuan/algorithm/sort/SortingTest.java
+++ src/test/java/com/albenyuan/algorithm/sort/SortingTest.java
@@ -1,13 +1,12 @@
 package com.albenyuan.algorithm.sort;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @Author Alben Yuan
@@ -18,31 +17,52 @@ public class SortingTest {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private Integer length = 100;
+    private Integer length = 100000;
+
+    private long before = 0l;
+
+    private long after = 0l;
+
+    private static List<Integer> DATA;
 
     private List<Integer> list = new ArrayList<>(length);
 
     @Before
     public void before() {
-        Random random = new Random();
-        for (int i = 0; i < length; i++) {
-            list.add(random.nextInt(length));
+        if (DATA == null) {
+            DATA = new ArrayList<>();
+            Random random = new Random();
+            for (int i = 0; i < length; i++) {
+                DATA.add(random.nextInt(length));
+            }
         }
+        list = new ArrayList<>(DATA);
+        before = System.currentTimeMillis();
+        logger.info("list  :{}", list);
+        logger.info("before:{}", before);
+    }
+
+    @After
+    public void after() {
+        after = System.currentTimeMillis();
+        logger.info("result:{}", list);
+        logger.info("after  :{}", after);
+        logger.info("times :{}", after - before);
     }
 
     @Test
     public void testInsertion() {
-        long before = System.currentTimeMillis();
-
-        logger.info("list  :{}", list);
-        logger.info("before:{}", before);
-        long end = System.currentTimeMillis();
-        InsertionSort insertionSort = new InsertionSort();
-        insertionSort.sort(list);
-        logger.info("result:{}", list);
-        logger.info("end   :{}", end);
-        logger.info("times :{}", end - before);
-
+        SortUtil.insertionSort(list);
     }
 
+
+    @Test
+    public void testBubbleSort() throws Exception {
+        SortUtil.bubbleSort(list);
+    }
+
+    @Test
+    public void testSelectionSort() throws Exception {
+        SortUtil.selectionSort(list);
+    }
 }
